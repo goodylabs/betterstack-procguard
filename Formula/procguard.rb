875b440bf1d1e6a8350b7619554ebdf357d54b77
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2025-12-11 14:38:32 +0100

    FIX: sha256 sum

--- Formula/procguard.rb
+++ Formula/procguard.rb
@@ -2,7 +2,7 @@ class Procguard < Formula
   desc "Bash process watchdog for Betterstack alerts"
   homepage "https://github.com/goodylabs/betterstack-procguard"
   url "https://github.com/goodylabs/betterstack-procguard/archive/refs/tags/v1.2.tar.gz"
-  sha256 "5420f2beac27e468526ad16c9024a3e3cb91b2f1bec58c8831ec36bf37f1519a"
+  sha256 "b9d916693b0a95a844493e2d622b0f42f886d5539f1a683839e65f4db8d5e605"
   license "MIT"
 
   def install
